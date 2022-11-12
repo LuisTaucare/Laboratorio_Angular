9d53c8bf1d6b878a493217c5167fec1a43c8b9a9
LuisTaucare <luis.taucare.villarreal@cftmail.cl>
2022-11-11 23:10:49 -0300

    laboratorio Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+
+const routes: Routes = [
+  {
+    path:"inicio",
+    component: InicioComponent  
+    
+  },
+  {
+    path: "nosotros",
+
+    component: NosotrosComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
